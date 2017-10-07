9a351b7284e97182ac464bf987d2593581684bfc
Strophon <admin@foodfeudgame.com>
2017-10-07 08:53:12 -0400

    Switch secureEqualsIgnoreCase to use uppercase

--- src/main/java/com/foodfeudgame/auth/EcdsaAuthProvider.java
+++ src/main/java/com/foodfeudgame/auth/EcdsaAuthProvider.java
@@ -121,8 +121,8 @@ public class EcdsaAuthProvider implements AuthProvider {
 	
 	private static boolean secureEqualsIgnoreCase(String ours, String theirs) {
 		// prevents information leakage due to more-equal values taking longer to compare
-		ours = ours.toLowerCase();
-		theirs = theirs.toLowerCase();
+		ours = ours.toUpperCase();
+		theirs = theirs.toUpperCase();
 		
 		boolean result = ours.length() == theirs.length();
 		int i = 0;
