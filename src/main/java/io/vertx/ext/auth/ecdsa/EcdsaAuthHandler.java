a2d15d48c480fe83d20a06b24a2a599cdeb6dfca
Strophon <admin@foodfeudgame.com>
2017-10-28 13:22:21 -0400

    Use AuthHandlerImpl.handle() w/ OPTIONS vuln; workaround: Router.options()

--- src/main/java/io/vertx/ext/auth/ecdsa/EcdsaAuthHandler.java
+++ src/main/java/io/vertx/ext/auth/ecdsa/EcdsaAuthHandler.java
@@ -1,7 +1,5 @@
 package io.vertx.ext.auth.ecdsa;
 
-import java.util.Base64;
-
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
 import io.vertx.core.Handler;
@@ -9,10 +7,10 @@ import io.vertx.core.http.HttpHeaders;
 import io.vertx.core.http.HttpServerRequest;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.auth.AuthProvider;
-import io.vertx.ext.auth.User;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.impl.AuthHandlerImpl;
-import io.vertx.ext.web.handler.impl.HttpStatusException;
+
+import java.util.Base64;
 
 public class EcdsaAuthHandler extends AuthHandlerImpl {
 
@@ -21,61 +19,6 @@ public class EcdsaAuthHandler extends AuthHandlerImpl {
 		boolean isEcdsa = authProvider instanceof EcdsaAuthProvider;
 		if(!isEcdsa)
 			throw new IllegalArgumentException("Must pass in an instance of EcdsaAuthProvider");
-	}
-
-	@Override
-	public void handle(RoutingContext context) {
-		User user = context.user();
-		if (user != null) {
-			// Already authenticated in, just authorize
-			authorize(user, authRes -> {
-				if(authRes.failed()) {
-					Throwable exception = authRes.cause();
-					if (exception != null && exception instanceof HttpStatusException) {
-						int statusCode = ((HttpStatusException) exception).getStatusCode();
-						context.fail(statusCode);
-						return;
-					}
-					context.fail(exception);
-					return;
-				}
-				
-				context.next();
-			});
-		} else {
-			parseCredentials(context, parseRes -> {
-				if(parseRes.failed()) {
-					context.fail(401);
-				}
-				if(context.failed()) {
-					return;
-				}
-				
-				JsonObject authInfo = parseRes.result();
-				authProvider.authenticate(authInfo, res -> {
-					if (res.succeeded()) {
-						User authenticated = res.result();
-						context.setUser(authenticated);
-						authorize(authenticated, authRes -> {
-							if(authRes.failed()) {
-								Throwable exception = authRes.cause();
-								if (exception != null && exception instanceof HttpStatusException) {
-									int statusCode = ((HttpStatusException) exception).getStatusCode();
-									context.fail(statusCode);
-									return;
-								}
-								context.fail(exception);
-								return;
-							}
-							
-							context.next();
-						});
-					} else {
-						context.fail(401);
-					}
-				});
-			});
-		}
 	}
 
 	@Override
